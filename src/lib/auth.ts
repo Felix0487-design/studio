7ee218b2009aa1204873a6fe53800451f382e061
Félix Romero <felix0487@gmail.com>
2025-10-06 10:22:04 +0000

    vale manten la contraseña pero sin espacios, esto funcionara en diez ter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ export const USERS = [
   'Ángel', 'Félix', 'Goyo', 'Toñi', 'Luis', 'José', 'Pepe', 'Lucio', 'Antonio', 'Virgilio'
 ];
 
-export const SUPER_SECRET_PASSWORD = 'navidad 2025';
+export const SUPER_SECRET_PASSWORD = 'navidad2025';
 
 export const SUPER_USER = 'jubilado';
 export const SUPER_USER_PASSWORD = 'navidad1960';
