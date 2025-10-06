5383ad490ad0c11353efbb7dd536f76c5cb87fe1
Félix Romero <felix0487@gmail.com>
2025-10-06 10:42:49 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,4 +2,11 @@
 // The Firebase configuration will be populated by the backend service.
 // Do not add any code to this file.
 //See the 'Firebase Usage Instructions' for more details on working with code scaffolding.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-2285238361-48e4c",
+  "appId": "1:682115661097:web:db65004cfbb0f02795cb38",
+  "apiKey": "AIzaSyA1QroHYn07ASbhz0Ad9zCgEWehoJi6uc8",
+  "authDomain": "studio-2285238361-48e4c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "682115661097"
+};
